a7c4cfe4b69426b63c526468ddd04fef23b70828
eth2353 <70237279+eth2353@users.noreply.github.com>
2022-03-31 19:17:37 +0200

    Minor improvements to balance indexer

--- src/indexer/balances.py
+++ src/indexer/balances.py
@@ -1,4 +1,5 @@
 import datetime
+import logging
 from contextlib import contextmanager
 
 import asyncio
@@ -9,13 +10,12 @@ from tqdm import tqdm
 from prometheus_client import start_http_server, Gauge
 
 from shared.setup_logging import setup_logging
-from shared.config import config
 from providers.beacon_node import BeaconNode, GENESIS_DATETIME
 from db.tables import Balance
+from db.db_helpers import get_db_uri
 
-
-logger = setup_logging(name=__file__)
-engine = create_engine(config["db"]["url"], executemany_mode="batch")
+logger = logging.getLogger(__name__)
+engine = create_engine(get_db_uri(), executemany_mode="batch")
 
 START_DATE = "2020-01-01"
 
@@ -47,11 +47,7 @@ async def index_balances():
     start_date = datetime.date.fromisoformat(START_DATE)
     end_date = datetime.date.today() + datetime.timedelta(days=1)
 
-    beacon_node = BeaconNode(
-        host=config["beacon_node"]["host"],
-        port=config["beacon_node"]["port"],
-        response_timeout=300,
-    )
+    beacon_node = BeaconNode()
 
     logger.info(f"Indexing balances for {len(pytz.common_timezones)} timezones.")
     slots_needed = set()
@@ -120,10 +116,11 @@ async def index_balances():
         for slot in tqdm(slots_needed):
             current_tx += 1
             # Store balances in DB
-            logger.debug(f"Executing insert statements for slot {slot}")
             balances_for_slot = await beacon_node.balances_for_slot(slot)
+            logger.debug(f"Executing insert statements for slot {slot}")
             if len(balances_for_slot) == 0:
                 # No balances available for slot (yet?), move on
+                logger.warning(f"No balances retrieved for slot {slot}")
                 continue
             session.execute(
                 "INSERT INTO balance(validator_index, slot, balance) VALUES(:validator_index, :slot, :balance)",
@@ -149,13 +146,11 @@ if __name__ == "__main__":
     # Start metrics server
     start_http_server(8000)
 
+    setup_logging()
+
     from time import sleep
 
-    loop = asyncio.get_event_loop()
-    try:
-        while True:
-            loop.run_until_complete(index_balances())
-            logger.info("Sleeping for a minute now")
-            sleep(60)
-    except KeyboardInterrupt:
-        loop.close()
+    while True:
+        asyncio.run(index_balances())
+        logger.info("Sleeping for a minute now")
+        sleep(60)
