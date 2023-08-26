b1268fca73bd66a8d7f7536bba2a5fb66fbdcff7
eth2353 <70237279+eth2353@users.noreply.github.com>
2023-08-26 14:31:19 +0200

    Add supported_currencies API endpoint

--- src/api/api_v1/api_.py
+++ src/api/api_v1/api_.py
@@ -1,8 +1,9 @@
 from fastapi import APIRouter
 
-from api.api_v1.endpoints import indexes, rewards, latest_block_rewards
+from api.api_v1.endpoints import currencies, indexes, rewards, latest_block_rewards
 
 api_v1_router = APIRouter(prefix="/api/v1")
+api_v1_router.include_router(currencies.router, tags=["currencies"])
 api_v1_router.include_router(indexes.router, tags=["indexes"])
 api_v1_router.include_router(rewards.router, tags=["rewards"])
 api_v1_router.include_router(latest_block_rewards.router, tags=["latest_block_rewards"])
