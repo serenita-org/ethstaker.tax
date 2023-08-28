24209460fb54daa4f1612706df3643ce61ef18af
eth2353 <70237279+eth2353@users.noreply.github.com>
2023-08-28 11:01:36 +0200

    Performance optimizations on v2 rewards endpoint

--- src/api/api_v2/endpoints/rewards.py
+++ src/api/api_v2/endpoints/rewards.py
@@ -36,7 +36,7 @@ async def rewards(
         raise HTTPException(
             status_code=400, detail=f"End date must be after start date"
         )
-    validator_indexes = set(rewards_request.validator_indexes)
+    validator_indexes = list(set(rewards_request.validator_indexes))
 
     # Cap end date at previous day - income and price for today (current day)
     # are not final yet
@@ -55,6 +55,11 @@ async def rewards(
         day=rewards_request.end_date.day,
         tzinfo=pytz.UTC,
     )
+    min_slot = beacon_node.slot_for_datetime(start_datetime)
+    max_slot = beacon_node.slot_for_datetime(datetime.datetime.combine(
+        end_datetime,
+        datetime.time(hour=23, minute=59, second=59, tzinfo=pytz.UTC)
+    ))
 
     # Check if we have execution layer rewards data for the requested validator indexes
     missing_exec_data = json.loads(await cache.get(CACHE_KEY_MISSING_DATA))
@@ -68,8 +73,8 @@ async def rewards(
             )
 
     # Let's get the rewards
-    first_slot_in_requested_period = BeaconNode.slot_for_datetime(dt=start_datetime)
-    activation_slots = await beacon_node.activation_slots_for_validators(validator_indexes=list(validator_indexes), cache=cache)
+    first_slot_in_requested_period = min_slot
+    activation_slots = await beacon_node.activation_slots_for_validators(validator_indexes=validator_indexes, cache=cache)
     consensus_layer_rewards: dict[int, list[RewardForDate]] = defaultdict(list)
     execution_layer_rewards: dict[int, list[RewardForDate]] = defaultdict(list)
     withdrawals: dict[int, list[RewardForDate]] = defaultdict(list)
@@ -96,7 +101,7 @@ async def rewards(
             )[0]
         initial_balances[validator_index] = initial_balance
 
-    # - Get end-of-day balances
+    # - Get all end-of-day balances
     range_day_count = (end_datetime - start_datetime).days + 1
     eod_slots = [
         beacon_node.slot_for_datetime(
@@ -110,6 +115,20 @@ async def rewards(
     ]
     eod_balances = db_provider.balances(slots=eod_slots, validator_indexes=validator_indexes)
 
+    # Get all withdrawals
+    all_withdrawals = db_provider.withdrawals(
+        min_slot=min_slot,
+        max_slot=max_slot,
+        validator_indexes=validator_indexes
+    )
+
+    # Get all block rewards
+    all_block_rewards = db_provider.block_rewards(
+        min_slot=min_slot,
+        max_slot=max_slot,
+        proposer_indexes=validator_indexes
+    )
+
     for validator_index in validator_indexes:
         prev_balance = initial_balances[validator_index]
         for eod_balance in eod_balances:
@@ -119,11 +138,9 @@ async def rewards(
 
             # Account for withdrawals
             amount_withdrawn_this_day_wei = Decimal(1e9) * sum(
-                w.amount_gwei for w in db_provider.withdrawals(
-                    min_slot=prev_balance.slot,
-                    max_slot=eod_balance.slot,
-                    validator_indexes=[validator_index]
-                )
+                w.amount_gwei for w in all_withdrawals
+                if w.validator_index == validator_index
+                and eod_balance.slot >= w.slot > prev_balance.slot
             )
             amount_earned_wei += amount_withdrawn_this_day_wei
 
@@ -146,17 +163,9 @@ async def rewards(
                 )
             prev_balance = eod_balance
 
-        block_rewards = db_provider.block_rewards(
-            min_slot=beacon_node.slot_for_datetime(start_datetime),
-            max_slot=beacon_node.slot_for_datetime(datetime.datetime.combine(
-                end_datetime,
-                datetime.time(hour=23, minute=59, second=59, tzinfo=pytz.UTC)
-            )),
-            proposer_indexes=[validator_index]
-        )
         # Sum in case multiple blocks proposed on same day
         exec_layer_rewards_for_date = defaultdict(int)
-        for br in block_rewards:
+        for br in [br for br in all_block_rewards if br.proposer_index == validator_index]:
             exec_layer_rewards_for_date[
                 beacon_node.datetime_for_slot(br.slot, pytz.UTC).date()
             ] += br.mev_reward_value_wei if br.mev else br.priority_fees_wei
