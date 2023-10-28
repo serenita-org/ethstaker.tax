27342c801f858cbfb135741c9a913a2ab359c40a
eth2353 <70237279+eth2353@users.noreply.github.com>
2023-10-28 15:37:17 +0200

    Improve DB schema

--- src/db/tables.py
+++ src/db/tables.py
@@ -30,11 +30,11 @@ class BlockReward(Base):
     reward_processed_ok = Column(Boolean, nullable=False)
 
 
-class RocketpoolMinipool(Base):
-    __tablename__ = "rocketpool_minipool"
+class RocketPoolMinipool(Base):
+    __tablename__ = "rocket_pool_minipool"
 
     minipool_index = Column(Integer, nullable=False, primary_key=True)
-    validator_index = Column(Integer, nullable=False)
+    validator_index = Column(Integer, nullable=False, index=True)
     node_address = Column(String(length=42), nullable=False)
     node_deposit_balance = Column(String(length=20), nullable=False)
     fee = Column(Numeric(precision=19), nullable=False)
@@ -44,20 +44,26 @@ class RocketpoolMinipool(Base):
     bond_pre_reduction_value_wei = Column(Numeric(precision=27), nullable=True)
 
 
-class RocketpoolReward(Base):
-    __tablename__ = "rocketpool_reward"
+class RocketPoolReward(Base):
+    __tablename__ = "rocket_pool_reward"
 
     node_address = Column(String(length=42), nullable=False, primary_key=True)
-    reward_period_index = Column(Integer, nullable=False, primary_key=True)
     reward_collateral_rpl = Column(Numeric(precision=27), nullable=False)
     reward_smoothing_pool_wei = Column(Numeric(precision=27), nullable=False)
 
+    # Relationships
+    reward_period_index = Column(ForeignKey("rocket_pool_reward_period.reward_period_index"), primary_key=True)
+    reward_period = relationship("RocketPoolRewardPeriod", back_populates="rewards")
 
-class RocketpoolRewardPeriod(Base):
-    __tablename__ = "rocketpool_reward_period"
+
+class RocketPoolRewardPeriod(Base):
+    __tablename__ = "rocket_pool_reward_period"
 
     reward_period_index = Column(Integer, nullable=False, primary_key=True)
     reward_period_end_time = Column(TIMESTAMP(timezone=True), nullable=False)
+
+    # Relationships
+    rewards = relationship("RocketPoolReward", back_populates="reward_period")
 
 
 class Withdrawal(Base):
